43d81c19c7b75811a9b2ed1cdf90e452fb7aa004
mmanzinvub <150946687+mmanzinvub@users.noreply.github.com>
2025-06-08 14:23:07 +0200

    Create camerasINSERT.sql

---
+++ projekt/camerasINSERT.sql
@@ -0,0 +1,20 @@
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/minolta-srt-101.png', 'SLR', 'Minolta SRT 101', 180.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/minolta-xe1.png', 'SLR', 'Minolta XE1', 180.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/canon-ae-1.png', 'SLR', 'Canon AE1', 200.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/canon-av-1.png', 'SLR', 'Canon AV1', 150.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/yashica-tl-electro-x.png', 'SLR', 'Yashica TL Electro X', 130.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/yashica-electro35-gsn.png', 'Rangefinder', 'Yashica Electro 35 GSN', 125.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/olympus-xa2.png', 'Point & Shoot', 'Olympus XA2', 80.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/pentax-67.png', 'Medium Format', 'Pentax 67', 750.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/polaroid-supercolor1000.png', 'Instant Camera', 'Polaroid Supercolor 1000', 75.00, 0);
+INSERT INTO cameras (image_url, category, name, price, stock)
+VALUES ('img/fujifilm-finepix-jv100.png', 'Digital Camera', 'Fujifilm FinePix JV100', 69.00, 0);
